5cf9468cc4853ad2b014ef868b3e281d923e8d47
Vadim Shpilevskiy <vshpilev@c2r1p11.hive.fi>
2024-09-02 14:26:39 +0300

    push_swap ver 0.0

---
+++ push_swap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vshpilev <vshpilev@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 13:17:46 by vshpilev          #+#    #+#             */
+/*   Updated: 2024/08/29 13:17:50 by vshpilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <unistd.h>
+
+typedef struct s_stack_node
+{
+	int					value;
+	int					current_position;
+	int					final_index;
+	int					push_price;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}				t_stack_node;
+
+#endif
